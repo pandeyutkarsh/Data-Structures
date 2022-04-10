ce2e67035e8d57029c58aa9a3e40177f23a3bfa7
Utkarsh Pandey <68511329+pandeyutkarsh@users.noreply.github.com>
2022-04-10 23:57:16 +0530

    Update Singly_Introduction.cpp

--- Linked List/Singly Linked List/Singly_Introduction.cpp
+++ Linked List/Singly Linked List/Singly_Introduction.cpp
@@ -12,17 +12,6 @@ class ListNode{
     }    
 };
 
-int Peek(ListNode* head){
-    ListNode* temp = head;
-    int count = 0;
-    while(temp != NULL){
-        count++;
-        temp = temp->next;
-    }
-
-    return count;
-}
-
 void printList(ListNode* node){
     while(node != NULL){
         cout<<node->data<<" ";
@@ -30,68 +19,15 @@ void printList(ListNode* node){
     }
 }
 
-void InsertAtHead(ListNode* &head,int data){
-
-    ListNode* node = new ListNode(data);
-    if(head == NULL){
-        head = node;
-    }else{
-        node->next = head;
-        head = node;
-    }
-}
-
-void InsertAtTail(ListNode* &head,int data){
-     ListNode* node = new ListNode(data);
-    if(head == NULL){
-        head = node;
-    }else{
-       ListNode* temp = head;
-       ListNode* prev = NULL;
-       while(temp != NULL){
-           prev = temp;
-           temp = temp->next;
-       }
-        prev->next = node;  
-    }
-}
-
-void InsertAtPosition(ListNode* &head,int data,int pos){
-
-    ListNode* newNode = new ListNode(data);
-
- if(head == NULL){
-     return;
- }else{
-
-     int cnt = 1;
-     ListNode* node = head;
-     while(node != NULL){
-         if(cnt == pos-1){
-             break;
-         }
-         cnt++;
-         node = node->next;
-     }
-
-     ListNode* forward = node->next;
-     node->next = newNode;
-     newNode->next = forward;
-
- }
-
-}
-
 int main(){
 
-  ListNode* head = NULL;
-    InsertAtHead(head,20);
-    InsertAtHead(head,10);
-    InsertAtHead(head,0);
-    InsertAtTail(head,-1);
-    InsertAtTail(head,-2);
-    InsertAtTail(head,-3);
-    InsertAtPosition(head,-30,3);
+    ListNode* head = new ListNode(1);
+    ListNode* middle = new ListNode(2);
+    ListNode* tail = new ListNode(3);
+
+    head->next = middle;
+    middle->next = tail;
+
     printList(head);
 
 }
