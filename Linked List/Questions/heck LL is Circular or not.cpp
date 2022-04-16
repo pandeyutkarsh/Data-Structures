76ef21855bbbe8c614e22fc80d604f6e71b941c6
Utkarsh Pandey <68511329+pandeyutkarsh@users.noreply.github.com>
2022-04-16 23:38:11 +0530

    Create heck LL is Circular or not.cpp

---
+++ Linked List/Questions/heck LL is Circular or not.cpp
@@ -0,0 +1,195 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+    public:
+        int data;
+        ListNode* next;
+
+    ListNode(int data){
+        this->data = data;
+        this->next = NULL;
+    } 
+
+    ~ListNode() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }   
+};
+
+int Peek(ListNode* head){
+    ListNode* temp = head;
+    int count = 0;
+    while(temp != NULL){
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+void printList(ListNode* node){
+    ListNode* temp = node;
+    while(node != NULL){
+        cout<<node->data<<" ";
+        node = node->next;
+        if(temp->data == node->next->data){
+            break;
+        }
+    }cout<<"\n";
+}
+
+void InsertAtHead(ListNode* &head,int data){
+
+    ListNode* node = new ListNode(data);
+    if(head == NULL){
+        head = node;
+    }else{
+        node->next = head;
+        head = node;
+    }
+}
+
+void InsertAtTail(ListNode* &head,int data){
+     ListNode* node = new ListNode(data);
+    if(head == NULL){
+        head = node;
+    }else{
+       ListNode* temp = head;
+       ListNode* prev = NULL;
+       while(temp != NULL){
+           prev = temp;
+           temp = temp->next;
+       }
+        prev->next = node; 
+        node->next = head; 
+    }
+}
+
+void InsertAtPosition(ListNode* &head,int data,int pos){
+
+ if(head == NULL){
+     return;
+ }else if(pos == 1){
+        InsertAtHead(head,data);
+ }else{
+     ListNode* newNode = new ListNode(data);
+     int cnt = 1;
+     ListNode* node = head;
+     while(node != NULL){
+         if(cnt == pos-1){
+             break;
+         }
+         cnt++;
+         node = node->next;
+     }
+
+     ListNode* forward = node->next;
+     node->next = newNode;
+     newNode->next = forward;
+
+ }
+}
+
+
+void Delete(ListNode* &head,int pos){
+    if(head == NULL){
+        return;
+    }else if(pos == 1){
+       ListNode* temp = head;
+        head = head -> next;
+        //memory free start ndoe
+        temp -> next = NULL;
+        delete temp;
+    }else{
+
+     int cnt = 1;
+     ListNode* curr = head;
+     ListNode* prev = NULL;
+      while(cnt < pos) {
+            prev = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+
+   prev -> next = curr -> next;
+        curr -> next  = NULL;
+        delete curr;
+
+    }
+}
+
+void reverse(ListNode* &head){
+    ListNode* curr = head;
+    ListNode* prev = NULL;
+    ListNode* forward = NULL;
+    int cnt = Peek(head)/2;
+
+    while(cnt > 0) {
+        forward = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward;
+        cnt--;
+    }
+
+    head->next = forward;
+    head = prev;
+
+}
+
+bool isCircular(ListNode* head){
+    // Write your code here.
+	ListNode* temp = head;
+	int flag = 0;
+	while(temp != NULL){
+		if(head->data == temp->next->data){
+			flag = 1;
+			break;
+		}
+        temp = temp->next;
+	}
+	
+	if(flag == 1){
+		return true  ;
+	}
+	
+	return false;
+}
+
+
+int main(){
+
+  ListNode* head = NULL;
+    // InsertAtHead(head,20);
+    // InsertAtHead(head,10);
+    // InsertAtHead(head,0);
+    // InsertAtTail(head,-1);
+    // InsertAtTail(head,-2);
+    // InsertAtTail(head,-3);
+    // InsertAtPosition(head,-30,3);
+    // printList(head);
+    // Delete(head,3);
+    ListNode* one = new ListNode(1);
+    ListNode* two = new ListNode(2);
+    ListNode* three = new ListNode(3);
+
+    one->next = two;
+    two->next = three;
+    three->next = one;
+    head = one;
+    // printList(head);
+    bool ans = isCircular(head);
+    cout<<ans<<endl;
+    // reverse(head);
+
+    // printList(head);
+
+
+
+}
